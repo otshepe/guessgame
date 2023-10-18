080965924c3bd8fdb2f98f8d4ef9b941a9e8cf3a
otshepe <148326242+otshepe@users.noreply.github.com>
2023-10-18 11:47:52 +0200

    Create game.py

---
+++ game.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+secret_number=random.randint(1,100)
+
+attempts= 0
+maximum attempts= 2 
+score = 0
+leaderboards = []
+
+#Create a time function
+
+def timer(start_time)
+
+#Display Menu
+
+print("user menu")
+print("pick number")
+print("settings")
+print("profile")
+print("Exit")
+
+print("2.profile")
+
+
+if condition:
+refiloe=37
+if refiloe<37:
+print("iam young")
+else:
+print("i am aged")
+
+
+
+if guess=secret_number:
+print("congrats you've won")
+
+#[Define a function to calculate score
+
+def calculate_score(attempts):
