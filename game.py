a9d2f4f7ad08066cd3bf1d3a6846c6831d4485c6
otshepe <148326242+otshepe@users.noreply.github.com>
2023-10-18 12:55:56 +0200

    Update game.py

--- game.py
+++ game.py
@@ -38,3 +38,32 @@ print("congrats you've won")
 #[Define a function to calculate score
 
 def calculate_score(attempts):
+  return 10 * attempts
+  if attempts= 2 :
+  return calculate_score -5
+else:
+return calculate_score +10
+
+
+a=1
+b=2
+def add_number(a+b)
+result=a+b
+return result
+
+
+
+
+
+
+
+
+
+#Problem Statements
+#1.Imagine you are working on a project with a team, and need to be able to keep #track of all the tasks members do.How will you simplify this collaboration?
+
+#2. An educstor wants to engage learners of an ECD centre for special needs learners and teach them in a way that is entertaining and fun.
+#what programme can she use for this?
+
+#3. Picture yourself as a librarian managing a busy library.What program can you use to catalog books, track borrowing and provide an easy way for all library membere to access reasourses?
+
